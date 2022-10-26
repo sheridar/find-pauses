963ab028eb0afb731c9b49325f120c5ba9adfee3
Ryan Sheridan <ryan.sheridan@ucdenver.edu>
2022-10-26 13:14:58 -0600

    scripts for plots

---
+++ src/Rmds/knit_rmd.R
@@ -0,0 +1,25 @@
+library(rmarkdown)
+library(docopt)
+
+doc <- "Usage: knit_Rmd.R [--help] [--input INPUT] [--proj PROJ] [--output OUT]
+
+-i --input INPUT    path to rmarkdown
+-p --proj PROJ      name of project, this is used to name output file
+-o --output OUTPUT  path to directory to write output file in
+-h --help           display this help message"
+
+opts <- docopt(doc)
+
+print(opts)
+
+proj <- opts$proj
+
+output <- paste0(opts$output, "/", proj, "_analysis.html")
+
+# Render Rmd
+render(
+  input       = opts$input,
+  output_file = output
+)
+
+
