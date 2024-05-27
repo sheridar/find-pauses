c5f516aeac6bd2fab45e96b81c8940f067dda849
Ryan Sheridan <ryan.sheridan@ucdenver.edu>
2024-05-27 16:42:54 -0600

    allow genome specification

--- src/Rmds/knit_rmd.R
+++ src/Rmds/knit_rmd.R
@@ -4,10 +4,11 @@ library(rmarkdown)
 library(docopt)
 library(here)
 
-doc <- "Usage: knit_Rmd.R [--help] [--input INPUT] [--proj PROJ] [--output OUT]
+doc <- "Usage: knit_Rmd.R [--help] [--input INPUT] [--proj PROJ] [--genome GENOME] [--output OUT]
 
 -i --input INPUT    path to rmarkdown
 -p --proj PROJ      name of project, this is used to name output file
+-g --genome GENOME  path to genome specific config file
 -o --output OUTPUT  path to directory to write output file
 -h --help           display this help message"
 
@@ -16,6 +17,7 @@ opts <- docopt(doc)
 print(opts)
 
 proj    <- opts$proj
+genome  <- opts$genome
 res_dir <- opts$output
 ttl     <- paste0("<p style=font-size:45px;>", proj, " NET-seq analysis</p>")
 
@@ -25,6 +27,6 @@ output <- here(res_dir, paste0(proj, "_analysis.html"))
 render(
   input       = opts$input,
   output_file = output,
-  params      = list(title = ttl)
+  params      = list(title = ttl, genome = genome)
 )
 
