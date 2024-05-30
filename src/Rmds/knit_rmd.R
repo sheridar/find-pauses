3802f022e7ec7d7efc3b22c1f02097dbf9b82892
Ryan Sheridan <ryan.sheridan@ucdenver.edu>
2024-05-29 19:18:45 -0600

    plot bug

--- src/Rmds/knit_rmd.R
+++ src/Rmds/knit_rmd.R
@@ -1,16 +1,17 @@
 renv::use(
   lockfile = "renv.lock",
-  library = "/opt/conda/envs/find-pauses/lib/R/library"
+  library  = "/opt/conda/envs/find-pauses/lib/R/library"
 )
 
 library(rmarkdown)
 library(docopt)
 library(here)
 
-doc <- "Usage: knit_Rmd.R [--help] [--input INPUT] [--proj PROJ] [--genome GENOME] [--output OUT]
+doc <- "Usage: knit_Rmd.R [--help] [--input INPUT] [--proj PROJ] [--config CONFIG] [--genome GENOME] [--output OUT]
 
 -i --input INPUT    path to rmarkdown
 -p --proj PROJ      name of project, this is used to name output file
+-c --config CONFIG  path to config file
 -g --genome GENOME  path to genome specific config file
 -o --output OUTPUT  path to directory to write output file
 -h --help           display this help message"
@@ -20,6 +21,7 @@ opts <- docopt(doc)
 print(opts)
 
 proj    <- opts$proj
+config  <- opts$config
 genome  <- opts$genome
 res_dir <- opts$output
 ttl     <- paste0("<p style=font-size:45px;>", proj, " NET-seq analysis</p>")
@@ -30,6 +32,10 @@ output <- here(res_dir, paste0(proj, "_analysis.html"))
 render(
   input       = opts$input,
   output_file = output,
-  params      = list(title = ttl, genome = genome)
+  params      = list(
+    title  = ttl,
+    config = config,
+    genome = genome
+  )
 )
 
