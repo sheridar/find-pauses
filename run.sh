5ff3d489daef48513ab0fb280e810efdbf00176a
bberickson@gmail.com <bberickson@gmail.com>
2026-04-06 16:38:09 -0600

    convert to slurm

--- run.sh
+++ run.sh
@@ -2,26 +2,24 @@
 
 set -o pipefail -o errexit
 
-mkdir -p logs
+mkdir -p logs rule_logs
 
 
 # Set default inputs
 install=0
 dry_run=0
-snake_args='--jobs 100 --keep-going --retries 1'
-bind_dir='/beevol/home'
 
 
 # Parse arguments
 usage() {
     echo """
-This will submit the pipeline to the LSF job manager, before submitting
+This will submit the pipeline to the SLURM job manager, before submitting
 update the SAMPLES.yaml config file with the correct sample names and
 paths. It is also helpful to first run this script using the '-d' option
 to check for any potential issues with the input files.
 
 USAGE
-$0 [-h] [-d] [-i] [-a SNAKE_ARGS] [-b BIND_PATH]
+$0 [-h] [-d] [-i]
 
 OPTIONS
 -h, display this help message
@@ -30,16 +28,10 @@ OPTIONS
     this will result in the creation of a 'micromamba' folder in your
     home directory, this option only needs to be included for the first
     run
--a, additional arguments to pass to snakemake, these should be wrapped in
-    quotes, default arguments are:
-    '$snake_args' 
--b, directory path to bind to container, this is required for access to
-    files that are outside of the workflow directory, default path is:
-    '$bind_dir'
     """
 }
 
-while getopts ":hdia:b:" args
+while getopts ":hdi" args
 do
     case "$args" in
         h)
@@ -48,17 +40,14 @@ do
             ;;
         d) dry_run=1 ;;
         i) install=1 ;;
-        a) snake_args="$OPTARG" ;;
-        b) bind_dir="$OPTARG" ;;
         :)
             echo -e "\nERROR: -$OPTARG requires an argument"
-
             usage
-	        exit 1
+            exit 1
             ;;
-	    *) 
+        *)
             usage
-	        exit 1
+            exit 1
             ;;
     esac
 done
@@ -68,30 +57,19 @@ done
 run_snakemake() {
     local install="$1"
     local dry_run="$2"
-    local snake_args="$3"
-    local bind_dir="$4"
-    local env_name="$5"
-    local env_file="$6"
+    local env_name="$3"
+    local env_file="$4"
+    local ssh_key_dir="$5"
 
-    # Path to .ssh directory
-    # this is used to provide the container access to ssh keys for
-    # transferring files to amc-sandbox
-    local ssh_key_dir="$7"
+    # Build snakemake args
+    local snake_args=''
 
-    # Add dry run argument
     if [ "$dry_run" -eq 1 ]
     then
-        local snake_args="--dry-run --quiet $snake_args"
-    fi
-
-    # Check that --jobs is always provided
-    if [[ ! "$snake_args" == *"--jobs"* ]]
-    then
-        local snake_args="--jobs 100 $snake_args"
+        local snake_args="--dry-run --quiet"
     fi
 
     # Install micromamba environment
-    # combine curl command with dummy command or script exits after install
     if [ "$install" -eq 1 ]
     then
         if ! command -v micromamba &> /dev/null
@@ -100,21 +78,15 @@ run_snakemake() {
                 sleep
         fi
 
-        # initialize shell for micromamba
         export MAMBA_ROOT_PREFIX="${MAMBA_ROOT_PREFIX:=$HOME/micromamba}"
-
         eval "$(micromamba shell hook --shell bash)"
-
         micromamba create -y -n "$env_name" -f "$env_file"
     fi
 
-    # need to initialize here also in case user runs pipeline multiple
-    # times after install but before starting new shell
-    # MAMBA_ROOT_PREFIX is still unset after initializing
+    # Activate micromamba environment if available
     if command -v micromamba &> /dev/null
     then
         export MAMBA_ROOT_PREFIX="${MAMBA_ROOT_PREFIX:=$HOME/micromamba}"
-    
         eval "$(micromamba shell hook --shell bash)"
 
         if [ -d "$MAMBA_ROOT_PREFIX/envs/$env_name" ]
@@ -123,43 +95,31 @@ run_snakemake() {
         fi
     fi
 
-    # module load singularity
-
-    drmaa_args='
-        -J {params.job_name}
-        -oo {log.out} 
-        -eo {log.err} 
-        -R "rusage[mem={resources.mem_gb}] span[hosts=1]"
-        -M {resources.mem_gb}
-        -n {threads} '
-
     snakemake $snake_args \
+        --profile 'src/profiles/slurm' \
         --snakefile 'src/pipelines/net.snake' \
-        --use-singularity \
-        --singularity-args "--bind $bind_dir" \
-        --drmaa "$drmaa_args" \
-        --config SSH_KEY_DIR="$ssh_key_dir" \
-        --configfiles 'SAMPLES.yaml' 'src/configs/net.yaml' 'src/configs/pauses.yaml'
+        --configfiles 'SAMPLES.yaml' 'src/configs/net.yaml' 'src/configs/pauses.yaml' \
+        --config SSH_KEY_DIR="$ssh_key_dir"
 }
 
 
 # Run the pipeline
-# make variables available to bsub
 function_def=$(declare -f run_snakemake)
 
 export install
 export dry_run
-export snake_args
-export bind_dir
 export function_def
 
-bsub \
-    -J 'NET-seq' \
-    -o 'logs/net_%J.out' \
-    -e 'logs/net_%J.err' \
-    -R 'rusage[mem=4GB] span[hosts=1]' \
-    -M 4GB \
-    -n 1 <<EOF
+sbatch \
+    --job-name='NET-seq' \
+    --output='logs/net_%j.out' \
+    --error='logs/net_%j.err' \
+    --mem=4G \
+    --nodes=1 \
+    --ntasks=1 \
+    --partition=normal \
+    --qos=normal \
+    --time='1-00:00:00' <<EOF
 #! /usr/bin/env bash
 
 set -o nounset -o pipefail -o errexit -x
@@ -169,11 +129,7 @@ $function_def
 run_snakemake \
     "$install" \
     "$dry_run" \
-    "$snake_args" \
-    "$bind_dir" \
     'snakemake' \
     'env/snakemake.yml' \
     "$HOME/.ssh"
 EOF
-
-
